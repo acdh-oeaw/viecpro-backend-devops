ae652db0d88a6539ba81141897bdbbc70e6152d6
Gregor Pirgie <gregor.pirgie@oeaw.ac.at>
2024-01-20 09:54:50 +0000

    chore: wip functionsregister

---
+++ viecpro_function_register/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ViecproFunctionRegisterConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'viecpro_function_register'
---
+++ viecpro_function_register/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from apis_core.apis_vocabularies.models import VocabsBaseClass, PersonInstitutionRelation
+
+
+class ToplevelFunction(models.Model):
+    pass
+
+
+class FunctionType(VocabsBaseClass):
+    pass
+
+
+class AlternativeFunctionName(models.Model):
+    pass
