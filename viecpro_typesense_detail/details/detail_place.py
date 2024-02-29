13b7fea257ea75150d2cbad24008a80f95ff363f
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-02-29 10:56:02 +0100

    fix: add sameAs links to details
    Closes: #56

--- viecpro_typesense_detail/details/detail_place.py
+++ viecpro_typesense_detail/details/detail_place.py
@@ -93,6 +93,7 @@ def main(offset:int=0) -> Dict[str, Any]:
         res["object_id"] = str(instance.id)
         res["model"] = model.__name__
         res["ampel"] = ampel(instance)
+        res["sameAs"] = [uri.uri for uri in instance.uri_set.all() if not uri.uri.startswith("https://viecpro.acdh.oeaw.ac.at")]
         results.append(res)
 
     return {"schema":schema, "results":results}
