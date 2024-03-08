292fbc5351f2931b48abdc67ff5ffa98dc0d3103
Gregor Pirgie <gregor.pirgie@oeaw.ac.at>
2024-03-08 14:31:53 +0000

    chore(docstring): fix typo

--- dubletten_tool/src/classes/RemergeHelper.py
+++ dubletten_tool/src/classes/RemergeHelper.py
@@ -3,7 +3,7 @@
 class Merger: 
     """
     HelperClass Storing common logic and functions used in remerge workflows. 
-    Allows for creating seperate Workflows for different kind of remerges.
+    Allows for creating seperate Workflows for different kinds of remerges.
     """
 
     def __init__(self, *args, **kwargs):
