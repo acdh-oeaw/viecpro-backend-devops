17d2001acd0cbbbfe67e7cf5afebdf9d8a3dc46f
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2025-09-25 10:58:41 +0200

    fix: error in group merger function

--- apis/settings/apisviecpro_backend_prod.py
+++ apis/settings/apisviecpro_backend_prod.py
@@ -5,10 +5,10 @@ import os
 
 REDIS_HOST = "redis"
 CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
-CELERY_RESULT_BACKEND = 'django-db'
+CELERY_RESULT_BACKEND = "django-db"
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -30,16 +30,17 @@ APIS_SKOSMOS = {
 ALLOWED_HOSTS = re.sub(
     r"https?://",
     "",
-    os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,viecpro.acdh-dev.oeaw.ac.at,viecpro-dev.acdh-dev.oeaw.ac.at,acdh-cluster.arz.oeaw.ac.at,viecpro-backend.acdh-cluster-2.arz.oeaw.ac.at,viecpro-backend.acdh-ch-dev.oeaw.ac.at"),
-
+    os.environ.get(
+        "ALLOWED_HOSTS",
+        "localhost,127.0.0.1,viecpro.acdh-dev.oeaw.ac.at,viecpro-dev.acdh-dev.oeaw.ac.at,acdh-cluster.arz.oeaw.ac.at,viecpro-backend.acdh-cluster-2.arz.oeaw.ac.at,viecpro-backend.acdh-ch-dev.oeaw.ac.at",
+    ),
 ).split(",")
 
 
 # You need to allow '10.0.0.0/8' for service health checks.
 ALLOWED_CIDR_NETS = ["10.0.0.0/8", "127.0.0.0/8"]
 
-CSP_DEFAULT_SRC = CSP_DEFAULT_SRC + \
-    ("sharonchoong.github.io", "github.com/devongovett")
+CSP_DEFAULT_SRC = CSP_DEFAULT_SRC + ("sharonchoong.github.io", "github.com/devongovett")
 
 DEBUG = True
 DEV_VERSION = False
@@ -48,20 +49,30 @@ SPECTACULAR_SETTINGS["COMPONENT_SPLIT_REQUEST"] = True
 SPECTACULAR_SETTINGS["COMPONENT_NO_READ_ONLY_REQUIRED"] = True
 
 
-INSTALLED_APPS += ["django_extensions", "apis_import_project", "apis_bibsonomy", "apis_ampel",
-                   "dubletten_tool", "viecpro_hierarchy", "viecpro_typesense", "viecpro_typesense_detail", "django_celery_results"]
+INSTALLED_APPS += [
+    "django_extensions",
+    "apis_import_project",
+    "apis_bibsonomy",
+    "apis_ampel",
+    "dubletten_tool",
+    "viecpro_hierarchy",
+    "viecpro_typesense",
+    "viecpro_typesense_detail",
+    "django_celery_results",
+]
 INSTALLED_APPS += ["django_grouper"]
 
 
-DATABASES = {"default":
-             {
-                 "ENGINE": "django.db.backends.mysql",
-                 "NAME": os.getenv('MYSQL_DATABASE'),
-                 "USER": os.getenv('MYSQL_USER'),
-                 "PASSWORD": os.getenv('MYSQL_PASSWORD'),
-                 "HOST": os.getenv("MYSQL_HOST"),
-                 "PORT": "3306",
-             }}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("MYSQL_DATABASE"),
+        "USER": os.getenv("MYSQL_USER"),
+        "PASSWORD": os.getenv("MYSQL_PASSWORD"),
+        "HOST": os.getenv("MYSQL_HOST"),
+        "PORT": "3306",
+    }
+}
 
 
 LANGUAGE_CODE = "de"
@@ -78,7 +89,7 @@ APIS_RELATIONS_FILTER_EXCLUDE = [
     "*collection*",
     "*published*",
     "*_set",
-    "*_set__*"
+    "*_set__*",
     "_ptr",
     "baseclass",
     "*id",
@@ -117,31 +128,42 @@ viecpro_hierarchy_BASE_URI = "https://viecpro-dev.acdh-dev.oeaw.ac.at/"
 ROBOTS_TXT_FOLDER = os.path.join(BASE_DIR, "robots_template")
 
 # register above folder as a template-dir
-TEMPLATES[0]["DIRS"] += [ROBOTS_TXT_FOLDER, ]
+TEMPLATES[0]["DIRS"] += [
+    ROBOTS_TXT_FOLDER,
+]
 
 
-APIS_IMPORT_PROJECT_IIIF_BASE_URL = "https://iiif.acdh-dev.oeaw.ac.at/iiif/images/viecpro/"
+APIS_IMPORT_PROJECT_IIIF_BASE_URL = (
+    "https://iiif.acdh-dev.oeaw.ac.at/iiif/images/viecpro/"
+)
 
 
 # Bibsonomy Settings
-APIS_BIBSONOMY = [{
-    'type': 'zotero',  # or zotero
-    'url': 'https://api.zotero.org/',  # url of the bibsonomy instance or zotero.org
-    'user': os.getenv("ZOTERO_USER"),
-    'API key': os.getenv("ZOTERO_API_KEY"),
-    'group':  os.getenv("ZOTERO_GROUP"),
-}]
+APIS_BIBSONOMY = [
+    {
+        "type": "zotero",  # or zotero
+        "url": "https://api.zotero.org/",  # url of the bibsonomy instance or zotero.org
+        "user": os.getenv("ZOTERO_USER"),
+        "API key": os.getenv("ZOTERO_API_KEY"),
+        "group": os.getenv("ZOTERO_GROUP"),
+    }
+]
 
 DJANGO_TYPESENSE = {
     "collection_prefix": "viecpro_",
 }
 
 
-MIDDLEWARE += [
-]
+MIDDLEWARE += []
+
 
 def custom_group_filter(queryset, request):
     from apis_core.apis_entities.filters import get_list_filter_of_entity
-    return get_list_filter_of_entity(queryset.model.__name__)(request.GET, queryset=queryset).qs
+
+    return get_list_filter_of_entity(queryset.model.__name__)(
+        request.GET, queryset=queryset
+    ).qs
+
 
 GROUP_FILTER = custom_group_filter
+GROUP_MERGE_FUNCTION = "merge"
